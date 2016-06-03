cfb8586e133e046296cebfb506fc5396e62c132b
a-she <al@alswebdesigns.net>
2016-06-03 01:43:44 -0700

    +handlebars, local imgs & fontawesome lib

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-player1 = $('#player1');
-player2 = $('#player2');
+(function(my) {
 
+// Private data
+var player1 = $('#player1');
+var player2 = $('#player2');
+
+var life_P1 = $('#life_P1');
+var life_P2 = $('#life_P2');
+
+/*** Robot models ***/
+var massStart = 3;
+var energyStart = 3;
+
+function makeData() {
+  this.mass = massStart;
+  this.energy = energyStart;
+}
+var p1_data = new makeData();
+var p2_data = new makeData();
+
+/*** Game Play ***/
 function changeTurn(playersTurn) {
   if (playersTurn === 1) {
     player2.removeClass('box-shadow-neon');
@@ -33,12 +51,12 @@ $('#player2-defense').click(function(){
   changeTurn(1);
 });
 
-(function(my) {
-  // Private data
-  var d;
+var template_life = $('#template_life');
+var fillTemplate_life = Handlebars.compile(template_life.html());
+life_P1.html(fillTemplate_life(p1_data));
+life_P2.html(fillTemplate_life(p2_data));
 
-  game = {};
+game = {};
+my.game = game;
 
-
-  my.game = game;
 })(window);
